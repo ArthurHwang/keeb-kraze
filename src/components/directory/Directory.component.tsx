7158d85ca9a25bd76dfd2f846f80eda18ce8693a
ArthurHwang <ahwang55@gmail.com>
2020-01-10 14:33:29 -0800

    swapped images to kb images

--- src/components/directory/Directory.component.tsx
+++ src/components/directory/Directory.component.tsx
@@ -6,35 +6,40 @@ export const Directory: React.FC = () => {
   const [sections] = useState([
     {
       title: 'keyboards',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      imageUrl:
+        'https://live.staticflickr.com/5826/30295544320_68b9b0d6b5_b.jpg',
       id: 1,
       linkUrl: 'shop/keyboards'
     },
     {
       title: 'keycaps',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      imageUrl:
+        'https://i.pinimg.com/originals/93/6a/fc/936afc46bac9f85de22966c88677217d.jpg',
       id: 2,
       linkUrl: 'shop/keycaps'
     },
     {
       title: 'switches',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      imageUrl:
+        'https://cdn.shopify.com/s/files/1/3099/8088/products/Gateron_Silent_Red_1024x1024@2x.jpg?v=1558704648',
       id: 3,
       linkUrl: 'shop/switches'
     },
     {
       title: 'accessories',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+      imageUrl:
+        'https://images.idgesg.net/images/article/2018/07/img_20180706_202831-100764491-large.jpg',
       size: 'large',
       id: 4,
       linkUrl: 'shop/accessories'
     },
     {
-      title: 'diy parts',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      title: 'parts',
+      imageUrl:
+        'https://cdn.shopify.com/s/files/1/1674/0405/products/sp111_plate_768x.png?v=1575664053',
       size: 'large',
       id: 5,
-      linkUrl: 'shop/diy-parts'
+      linkUrl: 'shop/parts'
     }
   ]);
 
