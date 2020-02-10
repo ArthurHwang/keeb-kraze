971859d0b0acaf59b356725532ede8afedb87802
ArthurHwang <ahwang55@gmail.com>
2020-02-09 16:11:45 -0800

    test remove SSL packages

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
 const compression = require("compression");
-const enforce = require("express-sslify");
-const sslRedirect = require("heroku-ssl-redirect");
+// const enforce = require("express-sslify");
+// const sslRedirect = require("heroku-ssl-redirect");
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({
@@ -19,8 +19,8 @@ const port = process.env.PORT || 5000;
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
-app.use(sslRedirect());
+// app.use(enforce.HTTPS({ trustProtoHeader: true }));
+// app.use(sslRedirect());
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
